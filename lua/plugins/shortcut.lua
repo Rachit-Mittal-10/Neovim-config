9339e10e60dba523f2ce13cffc96a4efe367a50d
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-06-08 19:04:39 +0530

    shortcut changed

--- lua/plugins/shortcut.lua
+++ lua/plugins/shortcut.lua
@@ -11,13 +11,18 @@ vim.keymap.set('n', '<C-u>', '<<', {desc='Un-indent the line', silent=true})
 vim.keymap.set('n', 'q', ':qa<CR>', {desc = 'Quit', silent=true})
 
 -- Terminal Shortcut
-vim.keymap.set('n', 'tt', ':below split | terminal<CR>', {desc="Open the Terminal in bottom half", silent=true})
-
+vim.keymap.set('n', 'tt', ':below split | terminal<CR> | i<CR>', {desc="Open the Terminal in bottom half", silent=true})
+vim.keymap.set('t','<C-d>',[[<C-\><C-n>]],{desc="Close the terminal",silent=true})
 -- New File Shortcut
 vim.keymap.set('n', 'e', ":ene <BAR> startinsert <CR>", {desc="New File", silent=true})
 
--- Tab shortcut
-vim.keymap.set('n','<C-Tab>',":bnext<CR>",{desc="Tab Further", silent=true})
-vim.keymap.set('n','<S-Tab>',":bprev<CR>",{desc="Tab Previous", silent=true})
+-- Buffer Change commands
+vim.keymap.set('n','L',":bnext<CR>",{desc="Tab Further", silent=true})
+vim.keymap.set('n','H',":bprev<CR>",{desc="Tab Previous", silent=true})
+vim.keymap.set('n','B',":buffers<CR>",{desc="Show Buffers",silent=true})
+
+-- Telescope keymaps
+vim.keymap.set('n','<leader>k',":Telescope keymaps<CR>",{desc="Show Keymaps",silent=true})
+
 
 return {}
