9fab4bf0f5fd9ccf444d0c419caaa1bad28d112a
Rachit <mittalrachit10063003@gmail.com>
2025-06-13 17:13:11 +0530

    lsp updated

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,44 @@
 return{
     {
         "williamboman/mason.nvim",
+        version = false,
         config = function()
             require("mason").setup()
         end
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        version = false,
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls","harper_ls","cssls","jdtls","eslint","jsonls","html","clangd"},
+                ensure_installed = { "lua_ls","cssls","jdtls","eslint","jsonls","html","clangd"},
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
+        version = false,
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim"
+        },
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({capabilities = capabilities})
-            lspconfig.harper_ls.setup({capabilities = capabilities})
-            lspconfig.cssls.setup({capabilities = capabilities})
-            lspconfig.eslint.setup({capabilities = capabilities})
-            lspconfig.html.setup({capabilities = capabilities})
-            lspconfig.clangd.setup({capabilities=capabilities})
-            vim.keymap.set('n','K',vim.lsp.buf.hover,{silent=true})
-            vim.keymap.set('n','gd',vim.lsp.buf.definition,{silent=true})
-            vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{silent=true})
+            local mason_lspconfig = require("mason-lspconfig")
+            local on_attach = function(_,bufnr)
+                local opts = {buffer=bufnr,silent=true}
+                vim.keymap.set('n','K',vim.lsp.buf.hover,{silent=true})
+                vim.keymap.set('n','gd',vim.lsp.buf.definition,{silent=true})
+                vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{silent=true})
+            end
+            local servers = {"lua_ls","cssls","jdtls","eslint","jsonls","html","clangd"}
+            for _,server in ipairs(servers) do
+                lspconfig[server].setup({
+                    capabilities=capabilities,
+                    on_attach = on_attach,
+                })
+            end
         end
     }
 }
